a42d640c34e0fad8e2774496aecddbe40fc0561e
tyler <kimtaesoo188@gmail.com>
2019-11-24 20:42:32 +0900

    Init main storyboard

---
+++ Example/MeccaTabBarViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MeccaTabbarViewController.swift
+//  Example
+//
+//  Created by tyler on 2019/11/24.
+//  Copyright © 2019 MeccaTabbar. All rights reserved.
+//
+
+import UIKit
+
+class MeccaTabBarViewController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.red
+    }
+}
